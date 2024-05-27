039dc3f37c8c993ddce51b7bd4e223ed2ddee239
Scr1amer <1156962061@qq.com>
2024-05-27 10:27:33 +0800

    Welcome page

--- Welcome.py
+++ Welcome.py
@@ -1,4 +1,12 @@
+'''Welcome/Project/Settings'''
 import streamlit as st
+import os
+import time
+
+project_path = os.getcwd() + "\\projects\\"
+exisiting_projects = os.listdir(project_path)
+st.session_state.project = None
+st.session_state.openai_key = None
 
 st.set_page_config(
     page_title="Welcome",
@@ -8,17 +16,28 @@ st.set_page_config(
 
 st.title('Welcome to Empirical Research Assistant!👋')
 
+@st.experimental_dialog("Create a new project")
+def create_project():
+    new_project_name = st.text_input("Project name:")
+    if st.button("Submit"):
+        if not os.path.exists(project_path + new_project_name):
+            os.makedirs(project_path + new_project_name)
+            st.success(f"Project '{new_project_name}' created successfully!")
+        else:
+            st.error(f"Project '{new_project_name}' already exists!") 
+        
 st.header("Create or Load a Project to Get Started!")
-
+st.write("The working directory is: ", project_path)
+st.session_state.project = st.selectbox(
+    "Select a project:",
+    exisiting_projects)
+st.write("Now the project is: ", st.session_state.project)
 if st.button("Create New Project"):
-    # Create a popup to ask the user for the project name
-    project_name = st.text_input("Enter the project name", "Enter to confirm")
-    if project_name:
-        pass
+    create_project()
 
-if st.button("Select Project from Directory"):
-    # Popup to ask the user to select a project from the directory
-    pass
-    
+st.divider()    
 
-# st.sidebar.success("Select a demo above.")
+st.header("Settings")
+st.session_state.openai_key = st.text_input("OpenAI API Key", type="password")
+               
+
